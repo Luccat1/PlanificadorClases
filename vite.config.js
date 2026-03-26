cff26b0bc37bc379e90fe8b5104d55fa94dbad03
Luciano Cataldo <lcataldoalvarado@gmail.com>
2026-03-26 18:41:42 -0300

    build: adjust github pages base URL

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: './', // Ajusta al nombre de tu repositorio en GitHub
+    base: '/PlanificadorClases/', // Ajusta al nombre de tu repositorio en GitHub
     test: {
         environment: 'jsdom',
         globals: true,
